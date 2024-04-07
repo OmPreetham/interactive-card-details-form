9eccbba98b609d248ec97583dc33350cc1b7d9e1
Om Preetham <ompreethambandi@gmail.com>
2024-04-06 20:34:14 -0500

    designed form layout

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,68 +2,73 @@ const Form = () => {
   return (
     <div className="form-wrapper">
       <form className="card" action="">
-        <div className="card__name-wrapper">
-          <label className="card__name" htmlFor="name">
+        <div className="card__block-wrapper">
+          <label className="card__label" htmlFor="name">
             Cardholder Name
           </label>
           <input
-            className="card__input-name"
+            className="card__input"
             type="text"
             name="name"
             id="name"
+            placeholder="e.g. Jane Appleseed"
           />
         </div>
-        <div className="card__number-wrapper">
-          <label className="card__number" htmlFor="number">
+        <div className="card__block-wrapper">
+          <label className="card__label" htmlFor="number">
             Card Number
           </label>
           <input
-            className="card__input-number"
+            className="card__input"
             type="number"
             name="number"
             id="number"
+            min={0}
+            placeholder="e.g. 1234 5678 9123 0000"
           />
         </div>
         <div className="card__extra-wrapper">
-          <div className="card__exp-wrapper">
-            <p className="card__exp-name">Exp. Date (MM/YY)</p>
-            <div className="card__month-wrapper">
-              <label className="card__month" htmlFor="month">
-                Month
-              </label>
+          <div className="card__block-wrapper">
+            <p className="card__label">Exp. Date (MM/YY)</p>
+            <div className="card__exp-wrapper">
               <input
-                className="card__input-month"
+                className="card__input"
                 type="number"
                 name="month"
                 id="month"
+                min={0}
+                max={12}
+                placeholder="MM"
               />
-            </div>
-            <div className="card__year-wrapper">
-              <label className="card__year" htmlFor="year">
-                Year
-              </label>
+
               <input
-                className="card__input-year"
+                className="card__input"
                 type="number"
                 name="year"
                 id="year"
+                min={0}
+                max={99}
+                placeholder="YY"
               />
             </div>
           </div>
-          <div className="card__cvv-wrapper">
-            <label className="card__cvv" htmlFor="cvv">
-              CVV
+          <div className="card__block-wrapper">
+            <label className="card__label" htmlFor="cvv">
+              CVC
             </label>
             <input
-              className="card__input-cvv"
+              className="card__input"
               type="number"
               name="cvv"
               id="cvv"
+              min={0}
+              max={9999}
+              placeholder="e.g. 123"
             />
           </div>
         </div>
         <button className="card__submit" type="submit">
-          Confrim
+          Confirm
         </button>
       </form>
     </div>
